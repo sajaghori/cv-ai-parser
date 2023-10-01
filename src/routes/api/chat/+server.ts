1a91715f3fd991c7c645d78c00cd3f4e5c1d3f08
Sasan Jaghori <sasan.jaghori@iteratec.com>
2023-10-02 00:22:52 +0200

    change model

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -1,21 +1,19 @@
-import { OpenAI } from 'openai'
-import { OPENAI_API_KEY } from '$env/static/private'
+import { openai } from '$lib/server/openai'
+import type { ChatCompletion } from 'openai/resources/chat'
 import type { RequestHandler } from './$types'
-
-const openai = new OpenAI({
-	apiKey: OPENAI_API_KEY
-})
 
 export const POST: RequestHandler = async ({ request }) => {
 	const { content } = (await request.json()) as { content: string }
+	const start = performance.now()
 	console.info('Chat endpoint called')
 
-	const chatCompletion = await openai.chat.completions.create({
-		model: 'gpt-3.5-turbo',
-		messages: [
-			{
-				role: 'system',
-				content: `You are a CV Parser, that returns a JSON response. Here is the raw CV content:
+	const chatCompletion: ChatCompletion = (await openai.chat.completions
+		.create({
+			model: 'gpt-3.5-turbo-16k',
+			messages: [
+				{
+					role: 'system',
+					content: `You are a CV Parser, that returns a JSON response. Here is the raw CV content:
 			\`\`\`
 
 			${content}
@@ -46,11 +44,26 @@ export const POST: RequestHandler = async ({ request }) => {
 			]. Can you parse it into a JSON? Return only the JSON. Please sort show the recent experiences and educations first. 
 			Be sure that the job title is valid, e.g. "Software Engineer" is valid, but "Software" is not. 
 			If the job title is not valid, then do not return it.`
-			}
-		]
-	})
+				}
+			]
+		})
+		.catch((error) => {
+			console.error('Chat request failed', error)
 
-	console.info('Chat request completed successfully')
+			return new Response(
+				JSON.stringify({
+					error: 'Chat request failed'
+				}),
+				{
+					headers: {
+						'content-type': 'application/json'
+					},
+					status: 500
+				}
+			)
+		})) as ChatCompletion
+
+	console.info('Chat request completed', performance.now() - start, 'ms')
 
 	return new Response(
 		JSON.stringify({
