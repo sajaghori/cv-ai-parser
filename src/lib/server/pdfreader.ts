efc7f1d2a3d8be5118c5de9d55a34b25ae4e5139
Sasan Jaghori <sasan.jaghori@iteratec.com>
2023-10-01 22:28:09 +0200

    wip: skip read errors

--- src/lib/server/pdfreader.ts
+++ src/lib/server/pdfreader.ts
@@ -1,14 +1,13 @@
 import { PdfReader } from 'pdfreader'
 
 export const readPdf = (file: Buffer): Promise<string> => {
-	return new Promise<string>((resolve, reject) => {
+	return new Promise<string>((resolve, _) => {
 		const pdfParser = new PdfReader({ debug: true })
 		let text = ''
 
 		pdfParser.parseBuffer(file, (err, item) => {
 			if (err) {
 				console.error('error:', err)
-				reject(err)
 			} else if (!item) {
 				console.warn('end of file')
 				resolve(text)
